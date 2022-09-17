a8bf82558ddff1f7d5eb818ee2fb5b0476abf45f
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2022-09-17 04:24:52 -0300

    all files mobile expo

---
+++ mobile/src/components/DuoCard/index.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { THEME } from '../../theme';
+import { DuoInfo } from '../DuoInfo';
+import { GameController } from 'phosphor-react-native'
+import { styles } from './styles';
+export interface DuoCardProps {
+  id: string;
+  hourEnd: string;
+  hourStart: string;
+  name: string;
+  useVoiceChannel: boolean;
+  weekDays: string[];
+  yearsPlaying: number;
+}
+interface Props {
+  data: DuoCardProps;
+  onConnect: () => void;
+}
+
+export function DuoCard({ data, onConnect }: Props) {
+  return (
+    <View style={styles.container}>
+      <DuoInfo
+        label={"Nome"}
+        value={data.name}
+      />
+      <DuoInfo
+        label={"Tempo de Jogo"}
+        value={`${data.yearsPlaying} ano(s)`}
+      />
+      <DuoInfo
+        label={"Disponibilidade"}
+        value={`${data.weekDays.length} dia(s) \u2022 
+        ${data.hourStart} - ${data.hourEnd}`}
+      />
+      <DuoInfo
+        label={"Chamada de áudio?"}
+        value={data.useVoiceChannel ? "Sim" : "Não"}
+        colorValue={data.useVoiceChannel ? THEME.COLORS.SUCCESS : THEME.COLORS.ALERT}
+      />
+      <TouchableOpacity style={styles.button}
+      onPress={onConnect}>
+        <GameController
+          color={THEME.COLORS.TEXT}
+          size={20}
+        />
+        <Text style={styles.buttonTitle}>
+          Conectar
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
---
+++ mobile/src/components/GameCard/index.tsx
@@ -0,0 +1,44 @@
+import { LinearGradient } from 'expo-linear-gradient';
+import {
+    Text,
+    TouchableOpacity,
+    ImageBackground,
+    ImageSourcePropType,
+    TouchableOpacityProps,
+} from 'react-native';
+import { THEME } from '../../theme';
+
+import { styles } from './styles';
+
+export interface GameCardProps {
+    id: string;
+    title: string;
+    _count: {
+        ads: string;
+    },
+    bannerUrl: string;
+}
+interface Props extends TouchableOpacityProps {
+    data: GameCardProps;
+}
+export function GameCard({ data, ...rest }: Props) {
+    return (
+        <TouchableOpacity style={styles.container} {...rest}>
+            <ImageBackground
+                style={styles.cover}
+                source={{ uri: data.bannerUrl }} >
+                <LinearGradient
+                    colors={THEME.COLORS.FOOTER}
+                    style={styles.footer}>
+
+                    <Text style={styles.name}>
+                        {data.title}
+                    </Text>
+                    <Text style={styles.ads}>
+                        {data._count.ads} anúncios
+                    </Text>
+                </LinearGradient>
+            </ImageBackground>
+        </TouchableOpacity>
+    );
+}
